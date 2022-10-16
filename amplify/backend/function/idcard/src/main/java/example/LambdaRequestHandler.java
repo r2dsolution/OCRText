8548d49ae49956f33d6bc51b0fdd54dbafe53d7b
Tawatchai Radom <tawatchai@r2dsolution.com>
2022-10-16 14:29:28 +0700

    update java code

--- amplify/backend/function/idcard/src/main/java/example/LambdaRequestHandler.java
+++ amplify/backend/function/idcard/src/main/java/example/LambdaRequestHandler.java
@@ -8,11 +8,18 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
 public class LambdaRequestHandler implements RequestHandler<RequestClass, ResponseClass>{   
 
     public ResponseClass handleRequest(RequestClass request, Context context){
-        String greetingString = String.format("Hello %s", request.body);
-        String nameString = String.format("Hello %s", request.body);
-        String addressString = String.format("Hello %s", request.body);
-        String idcardnumberString = String.format("Hello %s", request.body);
-        return new ResponseClass(greetingString) ,(nameString) ,(addressString) ,(idcardnumberString);
+        String greeting = String.format("Hello %s", request.body);
+        String firstname = "name";
+        String lastname = "lastname";
+        String idCardNo = "123456789";
+        // ResponseClass response =  new ResponseClass();
+        // response.setFirstname(firstname);
+        // response.setLastname(lastname);
+        // response.setIdCardNo(idCardNo);
+        // response.setGreetings(greeting);
+
+        ResponseClass response =  new ResponseClass(greeting,firstname,lastname,idCardNo);
+        return response;
         
     }
 }
--- amplify/backend/function/idcard/src/main/java/example/ResponseClass.java
+++ amplify/backend/function/idcard/src/main/java/example/ResponseClass.java
@@ -4,6 +4,35 @@ package example;
         
      public class ResponseClass {
         String greetings;
+        String firstname;
+        String lastname;
+        String idCardNo;
+
+       
+
+        public String getIdCardNo() {
+            return idCardNo;
+        }
+
+        public void setIdCardNo(String idCardNo) {
+            this.idCardNo = idCardNo;
+        }
+
+        public String getLastname() {
+            return lastname;
+        }
+
+        public void setLastname(String lastname) {
+            this.lastname = lastname;
+        }
+
+        public String getFirstname() {
+            return firstname;
+        }
+
+        public void setFirstname(String firstname) {
+            this.firstname = firstname;
+        }
 
         public String getGreetings() {
             return this.greetings;
@@ -13,55 +42,15 @@ package example;
             this.greetings = greetings;
         }
 
-        public ResponseClass(String greetings) {
-            this.greetings = greetings;
+        public ResponseClass() {
+            
         }
-    }
     
-     public class ResponseClass {
-        String name;
-
-        public String getName() {
-            return this.name;
+        public ResponseClass(String greetings, String firstname, String lastname, String idCardNo) {
+            this.greetings = greetings;
+            this.firstname = firstname;
+            this.lastname = lastname;
+            this.idCardNo = idCardNo;
         }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public ResponseClass(String name) {
-            this.name = name;
-        }
-    }
-
-     public class ResponseClass {
-        String address;
-
-        public String getAddress() {
-            return this.address;
-        }
-
-        public void setAddress(String address) {
-            this.address = address;
-        }
-
-        public ResponseClass(String address) {
-            this.address = address;
-        }
-    }
-
-     public class ResponseClass {
-        String idcardnumber;
-
-        public String getIdcardnumber() {
-            return this.idcardnumber;
-        }
-
-        public void setIdcardnumber(String idcardnumber) {
-            this.idcardnumber = idcardnumber;
-        }
-
-        public ResponseClass(String idcardnumber) {
-            this.idcardnumber = idcardnumber;
-        }
+     
     }
